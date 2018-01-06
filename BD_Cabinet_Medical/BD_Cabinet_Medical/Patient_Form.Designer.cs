c3a644e73a4278d4f68c3c78c2a7892f387c7a6a
Daniela <Daniela@DESKTOP-3R1BJRO>
2018-01-06 05:05:38 +0200

    PatientForm Design

--- BD_Cabinet_Medical/BD_Cabinet_Medical/Patient_Form.Designer.cs
+++ BD_Cabinet_Medical/BD_Cabinet_Medical/Patient_Form.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Patient_Form));
             this.Logout_Button = new System.Windows.Forms.Button();
             this.nameLabel = new System.Windows.Forms.Label();
             this.newAppointment = new System.Windows.Forms.Button();
@@ -42,14 +43,17 @@
             this.cabinet_MedicalDataSet = new BD_Cabinet_Medical.Cabinet_MedicalDataSet();
             this.appointmentsBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.appointmentsTableAdapter = new BD_Cabinet_Medical.Cabinet_MedicalDataSetTableAdapters.AppointmentsTableAdapter();
+            this.Programare = new System.Windows.Forms.GroupBox();
+            this.showHistory = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.History)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.cabinet_MedicalDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.appointmentsBindingSource)).BeginInit();
+            this.Programare.SuspendLayout();
             this.SuspendLayout();
             // 
             // Logout_Button
             // 
-            this.Logout_Button.Location = new System.Drawing.Point(12, 326);
+            this.Logout_Button.Location = new System.Drawing.Point(12, 110);
             this.Logout_Button.Name = "Logout_Button";
             this.Logout_Button.Size = new System.Drawing.Size(75, 23);
             this.Logout_Button.TabIndex = 0;
@@ -61,7 +65,7 @@
             // 
             this.nameLabel.AutoSize = true;
             this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nameLabel.Location = new System.Drawing.Point(8, 9);
+            this.nameLabel.Location = new System.Drawing.Point(8, 46);
             this.nameLabel.Name = "nameLabel";
             this.nameLabel.Size = new System.Drawing.Size(65, 24);
             this.nameLabel.TabIndex = 1;
@@ -71,11 +75,12 @@
             // 
             this.newAppointment.AccessibleDescription = "";
             this.newAppointment.AllowDrop = true;
-            this.newAppointment.Location = new System.Drawing.Point(12, 64);
+            this.newAppointment.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("newAppointment.BackgroundImage")));
+            this.newAppointment.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.newAppointment.Location = new System.Drawing.Point(35, 23);
             this.newAppointment.Name = "newAppointment";
-            this.newAppointment.Size = new System.Drawing.Size(75, 23);
+            this.newAppointment.Size = new System.Drawing.Size(121, 109);
             this.newAppointment.TabIndex = 2;
-            this.newAppointment.Text = "New Appointment";
             this.newAppointment.UseVisualStyleBackColor = true;
             this.newAppointment.Click += new System.EventHandler(this.newAppointment_Click);
             // 
@@ -84,7 +89,7 @@
             this.dateTime.CustomFormat = "yyyy-MM-dd hh:mm:ss";
             this.dateTime.Enabled = false;
             this.dateTime.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTime.Location = new System.Drawing.Point(249, 121);
+            this.dateTime.Location = new System.Drawing.Point(303, 97);
             this.dateTime.MaxDate = new System.DateTime(2100, 12, 31, 0, 0, 0, 0);
             this.dateTime.MinDate = new System.DateTime(2018, 1, 15, 0, 0, 0, 0);
             this.dateTime.Name = "dateTime";
@@ -96,7 +101,7 @@
             // 
             this.medicLabel.AutoSize = true;
             this.medicLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.medicLabel.Location = new System.Drawing.Point(170, 66);
+            this.medicLabel.Location = new System.Drawing.Point(220, 40);
             this.medicLabel.Name = "medicLabel";
             this.medicLabel.Size = new System.Drawing.Size(60, 17);
             this.medicLabel.TabIndex = 5;
@@ -106,7 +111,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(170, 125);
+            this.label1.Location = new System.Drawing.Point(220, 97);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(47, 17);
             this.label1.TabIndex = 6;
@@ -116,7 +121,7 @@
             // 
             this.Doctors.Enabled = false;
             this.Doctors.FormattingEnabled = true;
-            this.Doctors.Location = new System.Drawing.Point(249, 64);
+            this.Doctors.Location = new System.Drawing.Point(303, 39);
             this.Doctors.Name = "Doctors";
             this.Doctors.Size = new System.Drawing.Size(225, 21);
             this.Doctors.TabIndex = 7;
@@ -124,7 +129,7 @@
             // saveAppointment
             // 
             this.saveAppointment.Enabled = false;
-            this.saveAppointment.Location = new System.Drawing.Point(399, 159);
+            this.saveAppointment.Location = new System.Drawing.Point(441, 134);
             this.saveAppointment.Name = "saveAppointment";
             this.saveAppointment.Size = new System.Drawing.Size(75, 23);
             this.saveAppointment.TabIndex = 8;
@@ -136,19 +141,20 @@
             // 
             this.History.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.History.Enabled = false;
-            this.History.Location = new System.Drawing.Point(173, 199);
+            this.History.Location = new System.Drawing.Point(130, 204);
             this.History.Name = "History";
             this.History.RowHeadersWidth = 100;
-            this.History.Size = new System.Drawing.Size(500, 150);
+            this.History.Size = new System.Drawing.Size(519, 150);
             this.History.TabIndex = 9;
             // 
             // historyButton
             // 
-            this.historyButton.Location = new System.Drawing.Point(12, 204);
+            this.historyButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("historyButton.BackgroundImage")));
+            this.historyButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.historyButton.Location = new System.Drawing.Point(13, 215);
             this.historyButton.Name = "historyButton";
-            this.historyButton.Size = new System.Drawing.Size(75, 23);
+            this.historyButton.Size = new System.Drawing.Size(74, 96);
             this.historyButton.TabIndex = 10;
-            this.historyButton.Text = "History";
             this.historyButton.UseVisualStyleBackColor = true;
             this.historyButton.Click += new System.EventHandler(this.historyButton_Click);
             // 
@@ -166,29 +172,54 @@
             // 
             this.appointmentsTableAdapter.ClearBeforeFill = true;
             // 
+            // Programare
+            // 
+            this.Programare.Controls.Add(this.label1);
+            this.Programare.Controls.Add(this.medicLabel);
+            this.Programare.Controls.Add(this.newAppointment);
+            this.Programare.Controls.Add(this.Doctors);
+            this.Programare.Controls.Add(this.saveAppointment);
+            this.Programare.Controls.Add(this.dateTime);
+            this.Programare.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Programare.Location = new System.Drawing.Point(130, 12);
+            this.Programare.Name = "Programare";
+            this.Programare.Size = new System.Drawing.Size(532, 163);
+            this.Programare.TabIndex = 12;
+            this.Programare.TabStop = false;
+            this.Programare.Text = "Programare noua";
+            // 
+            // showHistory
+            // 
+            this.showHistory.AutoSize = true;
+            this.showHistory.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.showHistory.Location = new System.Drawing.Point(21, 323);
+            this.showHistory.Name = "showHistory";
+            this.showHistory.Size = new System.Drawing.Size(52, 17);
+            this.showHistory.TabIndex = 13;
+            this.showHistory.Text = "Istoric";
+            // 
             // Patient_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(669, 373);
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(674, 373);
             this.ControlBox = false;
+            this.Controls.Add(this.showHistory);
+            this.Controls.Add(this.Programare);
             this.Controls.Add(this.historyButton);
             this.Controls.Add(this.History);
-            this.Controls.Add(this.saveAppointment);
-            this.Controls.Add(this.Doctors);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.medicLabel);
-            this.Controls.Add(this.dateTime);
-            this.Controls.Add(this.newAppointment);
             this.Controls.Add(this.nameLabel);
             this.Controls.Add(this.Logout_Button);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimizeBox = false;
             this.Name = "Patient_Form";
             this.Load += new System.EventHandler(this.Patient_Form_Load);
             ((System.ComponentModel.ISupportInitialize)(this.History)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.cabinet_MedicalDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.appointmentsBindingSource)).EndInit();
+            this.Programare.ResumeLayout(false);
+            this.Programare.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -209,5 +240,7 @@
         private Cabinet_MedicalDataSet cabinet_MedicalDataSet;
         private System.Windows.Forms.BindingSource appointmentsBindingSource;
         private Cabinet_MedicalDataSetTableAdapters.AppointmentsTableAdapter appointmentsTableAdapter;
+        private System.Windows.Forms.GroupBox Programare;
+        private System.Windows.Forms.Label showHistory;
     }
 }
